80765aad8771dc64a77bd45f06d02997e05b4813
Matt Lilley <m.k.lilley@gmail.com>
2022-12-12 15:50:53 +0000

    Adds proxy server files

---
+++ index.js
@@ -0,0 +1,25 @@
+// uses http-proxy-middleware V2
+// https://github.com/chimurai/http-proxy-middleware/tree/v2.0.4#readme
+
+require("dotenv").config();
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use(
+    createProxyMiddleware({
+        target: process.env.API,
+        logLevel: "debug",
+        changeOrigin: true,
+        logger: console,
+        onProxyRes: (proxyRes, req, res) => {
+            proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+        },
+        onError: (err, req, res) => {
+            console.log(err);
+        },
+    })
+);
+
+app.listen(process.env.PORT);
